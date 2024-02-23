298db8fd50fe18e3a384100452ad6c9419d8a267
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-02-23 13:40:41 +0100

    add light sync

---
+++ source/light_sync.c
@@ -0,0 +1,52 @@
+#include "light_sync.h"
+
+static bool buttom_lamp_states[N_FLOORS * N_BUTTONS] = {false};
+static bool stop_button_lamp_state = false;
+static int floor_indicator_state = 0;
+
+/**
+ * Turns of all lights
+*/
+void light_sync_init() {
+    log_info("Initializing lamps");
+
+    for(int floor = 0; floor < N_FLOORS; floor++) {
+        for(int button_type = 0; button_type < N_BUTTONS; button_type++) {
+            elevio_buttonLamp(floor, button_type, 0);
+        }
+    }
+
+    elevio_stopLamp(0);
+
+    elevio_floorIndicator(0);
+}
+
+/**
+ * Sets lights to correspond with order queue, stop button state and last floor.
+*/
+void light_sync() {
+    // Button lamps
+    for(int floor = 0; floor < N_FLOORS; floor++) {
+        for(int button_type = 0; button_type < N_BUTTONS; button_type++) {
+            bool existance = order_exists(floor, (button_type == BUTTON_HALL_UP) - (button_type == BUTTON_HALL_DOWN));
+
+            if(buttom_lamp_states[N_FLOORS*floor + button_type] != existance) {
+                elevio_buttonLamp(floor, button_type, existance);
+            }
+        }
+    }
+
+    // Stop button lamp
+    bool stop_button_state = input_stop_button_held();
+    if (stop_button_lamp_state != stop_button_state) {
+        stop_button_lamp_state = stop_button_state;
+        elevio_stopLamp(stop_button_lamp_state);
+    }
+
+    // Floor indicator lamps
+    int last_floor = get_last_floor();
+    if (floor_indicator_state != last_floor) {
+        floor_indicator_state = last_floor;
+        elevio_floorIndicator(floor_indicator_state);
+    }
+}
--- source/light_sync.h
+++ source/light_sync.h
@@ -1,3 +1,7 @@
 /** @file
  * @brief Lightsync module will synchronize the panel lights to the corresponding internal active inputs.
 */
+
+#include "queue.h"
+
+void light_sync();
