6fb9203ddfc2bf56e2e3d3237c2daa9db398cf89
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-02-23 10:25:26 +0100

    replace printf with log_debug

--- source/queue.c
+++ source/queue.c
@@ -54,20 +54,21 @@ void parse_input(int current_floor) {
 
   // If the order queue is empty, add the target floor to the queue
   if (orders_length() == 0) {
-    printf("Creating first order\n");
+    log_debug("Creating first order\n");
     order_insert_before(NULL, order_destination, order_direction);
     return;
   }
 
   order_element_t *head = orders.head;
   int current_direction = dir(head->floor, current_floor);
-  // printf("\n     Current floor: %d, Current direction: %d (%s)\n", current_floor, current_direction, current_direction == 0 ? "off" : current_direction > 0 ? "up" : "down");
-  // printf(  " Order destination: %d,   Order direction: %d (%s)\n", order_destination, order_direction, order_direction == 0 ? "off" : order_direction > 0 ? "up" : "down");
-  // printf  ("Action destination: %d,  Action direction: %d (%s)\n", head->floor,   current_direction, current_direction == 0 ? "off" : current_direction > 0 ? "up" : "down");
+  // log_debug("\n     Current floor: %d, Current direction: %d (%s)\n", current_floor, current_direction, current_direction == 0 ? "off" : current_direction > 0 ? "up" : "down");
+  // log_debug(  " Order destination: %d,   Order direction: %d (%s)\n", order_destination, order_direction, order_direction == 0 ? "off" : order_direction > 0 ? "up" : "down");
+  // log_debug  ("Action destination: %d,  Action direction: %d (%s)\n", head->floor,   current_direction, current_direction == 0 ? "off" : current_direction > 0 ? "up" : "down");
   
   // Check if the order already exists
   if (order_exists(order_destination, order_direction)) {
-    printf("Order already exists\n");
+    log
+    ("Order already exists\n");
     return;
   }
 
@@ -90,11 +91,11 @@ void parse_input(int current_floor) {
     int upper_limit = (relative_direction < 0) ? source->floor      : destination->floor;
     int destination_direction = dir(destination->floor, source->floor);
 
-    // printf("Relative direction: %d\n", relative_direction);
-    // printf("Lower limit: %d, Upper limit: %d, Order destination: %d\n", lower_limit, upper_limit, order_destination);
+    // log_debug("Relative direction: %d\n", relative_direction);
+    // log_debug("Lower limit: %d, Upper limit: %d, Order destination: %d\n", lower_limit, upper_limit, order_destination);
     if (order_destination >= lower_limit && order_destination <= upper_limit) {
       if (order_direction == destination_direction || order_direction == 0) {
-        printf("Order made along current path: %s", order_direction == 0 ? "to let someone off" : "in the same direction\n");
+        log_debug("Order made along current path: %s", order_direction == 0 ? "to let someone off" : "in the same direction\n");
         order_insert_before(destination, order_destination, order_direction);
         order_inserted = true;
         break;
@@ -108,7 +109,7 @@ void parse_input(int current_floor) {
 
   // If the order is not made along the current path, add it to the end of the queue
   if (!order_inserted) {
-    printf("Adding order to end of queue:\n");
+    log_debug("Adding order to end of queue:\n");
     order_insert_before(NULL, order_destination, order_direction);
   }
 }
@@ -123,14 +124,14 @@ void order_insert_before(order_element_t *reference_node, int floor, int directi
   // If the head is null (linked list is empty)
   order_element_t *node = orders.head;
   if (node == NULL) {
-    printf("Inserted order at head\n");
+    log_debug("Inserted order at head\n");
     orders.head = new_order;
     orders.tail = new_order;
   }
 
   // If the reference_node is NULL, insert at end
   else if (reference_node == NULL) {
-    printf("Inserted order at tail\n");
+    log_debug("Inserted order at tail\n");
     if (orders.tail != NULL) orders.tail->next = new_order;
     orders.tail = new_order;
     node = NULL; // Prevents the loop from running
@@ -139,14 +140,14 @@ void order_insert_before(order_element_t *reference_node, int floor, int directi
   // Iterate through linked list
   order_element_t *prev_node = orders.head;
   while (node != NULL) {
-    printf("Comparing: (%d, %s), (%d, %s)\n", node->floor, node->direction == 0 ? "s" : node->direction > 0 ? "u" : "d", reference_node->floor, reference_node->direction == 0 ? "s" : reference_node->direction > 0 ? "u" : "d");
+    log_debug("Comparing: (%d, %s), (%d, %s)\n", node->floor, node->direction == 0 ? "s" : node->direction > 0 ? "u" : "d", reference_node->floor, reference_node->direction == 0 ? "s" : reference_node->direction > 0 ? "u" : "d");
     if (order_identical(node, reference_node)) {
       if (node == orders.head) {
-        printf("Inserted order before head\n");
+        log_debug("Inserted order before head\n");
         new_order->next = node;
         orders.head = new_order;
       } else {
-        printf("Inserted order before floor: %d\n", reference_node->floor);
+        log_debug("Inserted order before floor: %d\n", reference_node->floor);
         new_order->next = node;
         prev_node->next = new_order;
       }
@@ -213,11 +214,11 @@ void orders_print() {
   order_element_t *node = orders.head;
   
   // Iterate through linked list
-  printf("Orders:\n");
+  log_debug("Orders:\n");
   while (node != NULL) {
     const char *description = node->direction == 0 ? "Then open" : node->direction > 0 ? "Then up" : "Then down";
-    printf("Move to %d, (%s)\n", node->floor, description);
+    log_debug("Move to %d, (%s)\n", node->floor, description);
     node = node->next;
   }
-  printf("\n");
+  log_debug("\n");
 }
