64c3ca1eb3245dd66680acdfb61bea9cf7d1d2ba
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-03-14 16:00:38 +0100

    refactor: unit tests main file

--- tests/unit/orders_tests.c
+++ tests/unit/orders_tests.c
@@ -18,17 +18,17 @@
 /*****************************************************************************/
 /* Set up/tear down                                                          */
 /*****************************************************************************/
-void setUp(void) {
+void orders_tests_set_up(void) {
     // This is run before EACH test
 }
-void tearDown(void) {
+void orders_tests_tear_down(void) {
     orders_clear_all();
 }
 
 /*****************************************************************************/
 /* Unit tests                                                                */
 /*****************************************************************************/
-void test_orders_add_and_get(void) {
+void test_add_and_get(void) {
     for (int floor = 0; floor < N_FLOORS; floor++) {
         for (int button = 0; button < N_BUTTONS; button++) {
             TEST_ASSERT_FALSE(orders_get(floor, button));
@@ -37,7 +37,7 @@ void test_orders_add_and_get(void) {
         }
     }
 }
-void test_orders_clear_all(void) {
+void test_clear_all(void) {
     for (int floor = 0; floor < N_FLOORS; floor++) {
         for (int button = 0; button < N_BUTTONS; button++) {
             orders_add(floor, button);
@@ -51,11 +51,7 @@ void test_orders_clear_all(void) {
     }
 }
 
-int main(void) {
-    UNITY_BEGIN();
-
-    RUN_TEST(test_orders_add_and_get);
-    RUN_TEST(test_orders_clear_all);
-
-    return UNITY_END();
+void orders_tests_run(void) {
+    RUN_TEST(test_add_and_get);
+    RUN_TEST(test_clear_all);
 }
